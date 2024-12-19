12f7588346c8af5b432963199d8608d2560571d9
Aalok <mail.aalokdas@gmail.com>
2024-12-19 12:53:30 +0530

    Few functions that I have added to check tthe data. linear model parameters are also present

--- Workbook_ISLR_Boston.R
+++ Workbook_ISLR_Boston.R
@@ -1,5 +1,16 @@
 library(MASS)
 library(ISLR2)
+#??ISLR2
+#View(Boston)
+##str(Boston)
+head(Boston)
 
-View(Auto)
-str(Auto)
+?lm()
+lm.fit<- lm(medv ~ lstat, Boston)
+lm.fit
+summary(lm.fit)
+names(lm.fit)
+lm.fit$coefficients
+coef(lm.fit)
+residuals(lm.fit)
+fitted.values(lm.fit)
