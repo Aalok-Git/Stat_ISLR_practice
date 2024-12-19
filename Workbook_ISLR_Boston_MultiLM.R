2d349daf59f00e7fc0b22455ec96ff064bdf117d
Aalok <mail.aalokdas@gmail.com>
2024-12-20 00:41:07 +0530

    Multiple linear regression on Boston data, with necessary statistics and comments

---
+++ Workbook_ISLR_Boston_MultiLM.R
@@ -0,0 +1,16 @@
+loadlibrary<- function(){
+  library(ISLR2)
+  library(MASS)
+  print("All the libraries are loaded")
+}
+loadlibrary()           #loads all the library with this function
+# View(Auto)
+attach(Boston)
+cor(medv, rm)             #gives us a rough estimate of the co-relation between different variables
+
+mod2<- lm(medv ~ lstat + rm)       #multiple LM for y= medv, and x1= lstat, x2= rm
+summary(mod2)
+# plot(medv,rm)
+# abline(mod2)
+
+mod3<- lm(medv ~ ., data= Boston)      #LM between all the variables 
